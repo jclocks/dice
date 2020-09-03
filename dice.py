e343270592263990b34d202dd0aa3d232fc96d99
Johnny Tirado <johnny.tirado.ii@gmail.com>
2020-09-03 09:31:16 -0400

    meh

--- dice.py
+++ dice.py
@@ -1,31 +1,40 @@
 #!/usr/bin/python3
 
-import random
+import random, sys, re, numpy
+from scipy.stats import mode
 
-# Flow:
-# Input
-    # Validate arguments if any
-        # Throw error if it's invalid
-    # Roll dice based on arguments
-        # if no args just do a 1d6
-    # Add stuff based on arguments
-        # Plus/minus
-        # High/low
-    # if flags:
-        # results
-        # mean
-        # median
-        # average
-# Output
-dice_results = []
-random.seed()
+def init():
+    global dice_results
+    dice_results = []
+    random.seed()
 
 def dice_roll(dice_count = 1, dice_size = 6):
     for i in range(0, dice_count):
         dice_results.append(random.randint(1, dice_size))
 
-def roll_sum():
-    return sum(dice_results)
+def dice_mean(array_in):
+    return int(numpy.mean(array_in).round())
 
+def dice_median(array_in):
+    return int(numpy.median(array_in))
+
+def dice_mode(array_in):
+    return scipy.stats.mode(array_in)[0][0]
+
+def launch_args(i):
+    if ('-r' in i) or ('--results' in i):
+        print('[Debug] Results flag')
+    if ('-m' in i) or ('--mean' in i):
+        print('[Debug] Mean flag')
+    if ('-d' in i) or ('--median' in i):
+        print('[Debug] Median flag')
+    if ('-o' in i) or ('--mode' in i):
+        print('[Debug] Mode flag')
+    if ('-g' in i) or ('--range' in i):
+        print('[Debug] Range flag')
+
+init()
 dice_roll()
-print(roll_sum())
+print(sum(dice_results))
+launch_args(sys.argv[1:])
+print(sys.argv[1:])
